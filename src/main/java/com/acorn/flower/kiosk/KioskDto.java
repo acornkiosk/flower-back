26d6751c2d55efdfbac387d0ce37f6a8294cb8c7
unso99 <unso99@naver.com>
2024-02-19 22:24:15 +0900

    Add: 키오스크 Dto 생성
    키오스크 Dto 생성
    
    Ref: #8

--- src/main/java/com/acorn/flower/kiosk/KioskDto.java
+++ src/main/java/com/acorn/flower/kiosk/KioskDto.java
@@ -1,5 +1,19 @@
 package com.acorn.flower.kiosk;
 
-public class KioskDto {
+import org.apache.ibatis.type.Alias;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Alias("kioskDto")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class KioskDto {
+	private int id;
+	private String location;
+	private String power;
 }
