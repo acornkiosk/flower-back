5d3f9cc9eb78cd89e45c9c2c85232083fcc4e0ed
unso99 <unso99@naver.com>
2024-03-05 12:06:07 +0900

    Add: 키오스크 관리 페이징 처리 api 추가

--- src/main/java/com/acorn/flower/kiosk/KioskService.java
+++ src/main/java/com/acorn/flower/kiosk/KioskService.java
@@ -5,11 +5,13 @@ import java.util.List;
 public interface KioskService {
 	public boolean insert(KioskDto dto);
 
-	public List<KioskDto> getList();
+	public List<KioskDto> getList(KioskDto dto);
 
 	public KioskDto getKiosk(KioskDto dto);
 
 	public boolean delete(KioskDto dto);
 
 	public boolean update(KioskDto dto);
+	
+	public KioskResponse selectPage(int pageNum);
 }
